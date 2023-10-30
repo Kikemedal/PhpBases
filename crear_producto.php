2b631012945e0ade6c0645f25b4e3ea12ef0e544
Enrique Medina <Kikemedal@gmail.com>
2023-10-30 12:08:40 +0000

    Subiendo crear_producto a la rama CrearProducto

---
+++ crear_producto.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lan="en">
+    <head>
+        <title> Formulario para crear producto </title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1> Formulario para crear Producto<h1>
+        <div>
+            <form>
+                <label>Introduce un nombre: </label><input type="text" id="nombre_producto">
+            </form>
+        </div>
+    </body>
+
+</html>
