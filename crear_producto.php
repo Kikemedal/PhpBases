3656c672f2084d736716aded6f62128b8745c547
Enrique Medina <Kikemedal@gmail.com>
2023-10-31 13:01:27 +0000

    Se han añadidio los campos del formulario

--- crear_producto.php
+++ crear_producto.php
@@ -11,10 +11,22 @@
         <meta charset="UTF-8">
     </head>
     <body>
-        <h1> Formulario para crear Producto<h1>
+        <h1> Formulario para crear Producto</h1>
         <div>
             <form>
                 <label>Introduce un nombre: </label><input type="text" id="nombre_producto">
+                <br>
+                <label>Introduce el precio en €: </label><input type="number" min="0.1" step="any" id="precio_producto">
+                <br>
+                <label>Introduce una imagen para el producto: </label> <input type="file" id="imagen_producto">
+                <br>
+                <label>Selecciona la categoría del producto: </label> 
+                <select id="categoria_producto">
+                    <option> Deportivo </option>
+                    <option> Diver </option>
+                    <option> Clasico </option>
+                    <option> Casual </option>
+                </select>
             </form>
         </div>
     </body>
