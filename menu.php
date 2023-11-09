2af638610e1609b20947a6bca0ddca3443048918
Enrique Medina <Kikemedal@gmail.com>
2023-11-09 12:46:10 +0000

    Se le han añadido las etiquetas <a> para poder enlazar a los ficheros php restantes en un futuro

--- menu.php
+++ menu.php
@@ -6,12 +6,23 @@
 
 <html>
     <head>
+        <meta charset="UTF-8">
+        <title> Menu principal </title>
 
     </head>
+
     <body>
         <header>
-            
+            <h1> Menu principal </h1>
         </header>
+        <nav>
+            <ul>
+                <li> <a href='/PhpBases/crear_producto.php'> Crea un producto </a></li>
+                <li> <a> Consulta un producto </a></li>
+                <li> <a> Modifica un producto </a></li>
+                <li> <a> Elimina un producto </a></li>
+            </ul>
+        </nav>
 
     </body> 
 
