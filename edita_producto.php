e764f44f84ddbb3c07052204a0217fb34443ff77
Enrique Medina <Kikemedal@gmail.com>
2023-11-13 18:47:22 +0000

    Subiendo el archivo

---
+++ edita_producto.php
@@ -0,0 +1,27 @@
+<?php 
+
+/*
+Modificar producto
+Se implementará en el fichero “edita_producto.php”
+Recibirá por GET el id del producto a modificar.
+Si no lo recibe, mostrará al usuario un desplegable para que escoja el producto a modificar.
+Mostrará los datos del producto en un formulario que el usuario podrá modificar y enviar a sí mismo para realizar la actualización de los datos en la tabla.
+*/
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
