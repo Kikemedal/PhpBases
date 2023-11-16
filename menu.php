b0e56172a69d7a58b4669a897c0cbf53eae72507
Enrique Medina <Kikemedal@gmail.com>
2023-11-16 13:52:21 +0000

    Se han puesto los enlaces a las respectivas paginas

--- menu.php
+++ menu.php
@@ -17,9 +17,9 @@
         </header>
         <nav>
             <ul>
-                <li> <a href='/PhpBases/crear_producto.php'> Crea un producto </a></li>
-                <li> <a href=''> Consulta un producto </a></li>
-                <li> <a href=''> Modifica un producto </a></li>
+                <li> <a href='crear_producto.php'> Crea un producto </a></li>
+                <li> <a href='listado_productos.php'> Consulta un producto </a></li>
+                <li> <a href='edita_producto.php'> Modifica un producto </a></li>
                 <li> <a href=''> Elimina un producto </a></li>
             </ul>
         </nav>
